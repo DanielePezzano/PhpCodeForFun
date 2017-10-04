3ffb95142ddca5e21c58b7d1e0f78cb3dead5cbd
Daniele <daniele.pezzano.consultant@yoox.com>
2017-10-04 11:27:12 +0200

    HashMd5Test
    Aggiunto un test Md5

--- EncryptionLibrary/tests/unit/HashTest.php
+++ EncryptionLibrary/tests/unit/HashTest.php
@@ -1,11 +1,10 @@
 <?php
-use EncriptionLib\KeyGenerator;
 use EncriptionLib\Client\EncryptClient;
 
 require_once('Client/EncryptClient.php');
 
 
-class EncryptTest extends \Codeception\Test\Unit
+class HashTest extends \Codeception\Test\Unit
 {
     /**
      * @var \UnitTester
@@ -33,4 +32,12 @@ class EncryptTest extends \Codeception\Test\Unit
         $encrypted = "28aaf057a501de45219fef77c4e298d2648b7e661c9b19265ab37ec69c849d08";
         $this->tester->assertEquals($encrypted, EncryptClient::CreateClient($plainText, $type, $method)->CryptMyData());
     }
+    
+    public function testMd5Hash(){
+        $type=EncryptClient::Hash;
+        $method = "md5";
+        $plainText = "Pzz!Dnl!0001";
+        $encrypted = "0d00b46abd0abdcd38846757261b2daf";
+        $this->tester->assertEquals($encrypted, EncryptClient::CreateClient($plainText, $type, $method)->CryptMyData());
+    }    
 }
